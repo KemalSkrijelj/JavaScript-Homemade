53453ba7f3231400fff8437badcdd323ac7b7027
Kemal Škrijelj <kemalskrijelj6@gmail.com>
2023-12-14 23:57:56 +0100

    Learning

---
+++ JavaScript-Homemade/concatination.js
@@ -0,0 +1,9 @@
+const bank1 = 100;
+const bank2 = 50;
+
+const allBanks = bank1 + bank2;
+const user = "Ed";
+
+const greeting = 'Hey there welcome to our ${user} ';
+
+
---
+++ JavaScript-Homemade/conditions.js
@@ -0,0 +1,38 @@
+// == Equal to 
+// 5 == 5 TRUE
+// 5 == 4 FALSE
+
+//Not equal
+// 4 != 3 TRUE
+// 4 != 4 FALSE
+
+// 5 > 10 FALSE
+// 10 > 6 TRUE
+// 10 >= 10 TRUE
+// 20 <= 20 TRUE
+
+// NEVER EVER USE DOUBLE EQUALS USE THE TRIPLE
+
+
+//const age = 15;
+//const bank = 20;
+
+//if (age >= 18){
+//     console.log('You are old enough  you can enter')
+//   
+// } else if (bank > 30){
+//     console.log ('Cool you have enough money')
+// else {
+//     console.log ('You are either too young or have no money')
+// }
+
+const name = null;
+
+if (name) {
+     console.log ('This value is truthy')
+} else {
+     console.log ('This value is falsey')
+}
+
+
+//FALSE, 0 , "", null, underfined, NaN

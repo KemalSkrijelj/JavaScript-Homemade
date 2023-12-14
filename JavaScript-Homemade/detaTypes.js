56705b3fd0fbe50bf76c515e93443abe5b55a3cc
Kemal Škrijelj <kemalskrijelj6@gmail.com>
2023-12-14 17:44:36 +0100

    .

---
+++ JavaScript-Homemade/detaTypes.js
@@ -0,0 +1,21 @@
+ //Primitive Data Types
+
+ //Numbers
+const day = 5;
+ //Strings
+const name = "Dev Ed";
+console.log (typeof name);
+
+ //BooLeans
+ const islogged = true;
+
+ //Undefined
+ let Day;
+ console.log (day);
+ //Null
+let minute = null;
+
+ //Symbol
+const Name = Symbol;
+
+//OBJECT 
---
+++ JavaScript-Homemade/veriables.js
@@ -0,0 +1,23 @@
+const inbox = 100;
+let user = "Ed";
+
+
+//camelCase
+let yourVideo = 20;
+let yourEmail = 30;
+
+let YourVideo = 20;
+let your_video = 20;
+
+let cat = "Meow";
+
+//Camel Case
+const yourVideos = 10;
+//Pascal Case
+const YourVideos = 20;
+//Underscore 
+const your_videos = 20;
+
+/* 
+
+*/

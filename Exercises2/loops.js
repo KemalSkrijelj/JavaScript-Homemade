4b6862c0eddbbc80a19ac590609d71a4ab18ccfc
Kemal Škrijelj <kemalskrijelj6@gmail.com>
2023-12-19 20:31:30 +0100

    Learning

--- Exercises2/loops.js
+++ Exercises2/loops.js
@@ -1,8 +1,52 @@
 //FOR LOOPS
 
-const texts = ['Singing the park', 'Watching myself dance',3333333,444444444444444]
+// const text = 'deved'
 
-for (let i = 0; i < texts.length; i++){
-  //RUN SOME CODE
-  console.log(texts[i]); 
+// for (let i = 0; i < texts.length; i++){
+//   //RUN SOME CODE
+//   console.log(texts[i]); 
+// }
+
+
+const names = ['Deved', 'Logic','Barbie','Tool','Mf Doom']
+
+//  for (let name of names) {
+//   if (name === 'Logic'){
+//     break;
+//   }
+//   console.log ('STAHPT DIS MADNESS');
+//  }
+
+//SPECIFICALLY FOR ARRWAYS
+// names.forEach(function(name, index) {
+//     if (name === 'Logic') {
+//       console.log('STAHPT DIS MADNESS')
+//       break;
+//     }
+//   })
+
+// const user = {
+//   name: 'Dev Ed',
+//   age: '25',
+//   subscribers: 200,
+//   money: 'lolno'
+// }
+
+// for(let x in user){
+//   console.log(user[x]);
+// }
+
+//WHILE, DO WHILE
+
+let i = 0;
+
+// while(20 < 30) {
+//     console.log(i);
+//     i += 2;
+// }
+
+do {
+    console.log (i)
+    i++;
 }
+while(i < 120);

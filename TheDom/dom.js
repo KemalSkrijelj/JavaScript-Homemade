9058b4fe449cd01fe51f4b55507190e30f40e0aa
Kemal Škrijelj <kemalskrijelj6@gmail.com>
2023-12-21 23:56:47 +0100

    ...

---
+++ TheDom/dom.js
@@ -0,0 +1,10 @@
+// const headers = document.getElementsByTagName("h2");
+// const list = document.getElementsByClassName("item");
+// const button = document.getElementById("submit");
+
+const headers = document.querySelector("h2");
+const list = document.querySelectorAll("#list a");
+
+console.log(list);
+
+

4c9d9655a8e9a00986c0e7e74ad9d8006adca493
Kemal Škrijelj <kemalskrijelj6@gmail.com>
2023-12-22 21:13:52 +0100

    Update

--- TheDom/dom.js
+++ TheDom/dom.js
@@ -1,10 +1,41 @@
-// const headers = document.getElementsByTagName("h2");
-// const list = document.getElementsByClassName("item");
-// const button = document.getElementById("submit");
+// const collectionsItems = document.getElementsByClassName("item");
+// const todoList = document.getElementById("todo-list");
+// const todoNr = document.getElementsByClassName("todo-nr")[0];
 
-const headers = document.querySelector("h2");
-const list = document.querySelectorAll("#list a");
+// const newItem = document.createElement('li');
+// newItem.classList.add("item");
+// newItem.innerText = "Item 3";
 
-console.log(list);
+// const anotherItem  = document.createElement("li");
+// anotherItem.classList.add("item");
+// anotherItem.innerText = "Item 4";
+
+// todoList.appendChild(newItem);
+// todoList.appendChild(anotherItem);
+
+// //Update the ammount of items that we have
+// todoNr.innerText + collectionsItems.length;
+
+// console.log(collectionsItems);
 
 
+const todoList = document.querySelector("#todo-list");
+const items = todoList.children();
+
+const todoNr = document.querySelector(".todo-nr");
+
+const newItem = document.createElement("li");
+newItem.classList.add("item");
+newItem.innerText = "Item 3";
+
+const anotherItem  = document.createElement("li");
+anotherItem.classList.add("item");
+anotherItem.innerText = "Item 4";
+
+todoList.appendChild(newItem);
+todoList.appendChild(anotherItem);
+
+//Update the ammount of items that we have
+todoNr.innerText = items.length;
+
+console.log(items);

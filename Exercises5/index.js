82a072bc2f2613975e96c3e9c11b72da85c95337
Kemal Škrijelj <kemalskrijelj6@gmail.com>
2024-01-14 22:14:10 +0100

    update

--- Exercises5/index.js
+++ Exercises5/index.js
@@ -54,30 +54,30 @@ console.log(izbrojane)
 //WHILE vezbe
 //Prebroj brojeve koji su deljivi sa 3
 
-let numbers = [1,2,3,4,5,11,22,33,44,55,123,234,345,450,510,1323421,421,21,412];
+// let numbers = [1,2,3,4,5,11,22,33,44,55,123,234,345,450,510,1323421,421,21,412];
 
-let arr = numbers.length//15
-let counter = 0;
+// let arr = numbers.length//15
+// let counter = 0;
 
-while(counter < arr){
-  if(numbers[counter] % 3 == 0){
-    console.log(`Brojevi  koji su deljivi3: ${numbers[counter]}`)
-  }
-  if(numbers[counter] % 2 == 0){
-    console.log(`Brojevi koji su deljivi2: ${numbers[counter]} `)
-  }
-  if(numbers[counter] % 5 == 0 ){
-    console.log(`Brojevi koji su deljiv5: ${numbers[counter]}`)
-  }
-  if(numbers[counter] % 21 == 0){
-    console.log(`Brojevi koji su deljivi21: ${numbers[counter]}`)
-  }
+// while(counter < arr){
+//   if(numbers[counter] % 3 == 0){
+//     console.log(`Brojevi  koji su deljivi3: ${numbers[counter]}`)
+//   }
+//   if(numbers[counter] % 2 == 0){
+//     console.log(`Brojevi koji su deljivi2: ${numbers[counter]} `)
+//   }
+//   if(numbers[counter] % 5 == 0 ){
+//     console.log(`Brojevi koji su deljiv5: ${numbers[counter]}`)
+//   }
+//   if(numbers[counter] % 21 == 0){
+//     console.log(`Brojevi koji su deljivi21: ${numbers[counter]}`)
+//   }
   
-  counter++;
-}
+//   counter++;
+// }
 
 
-
+//vezba2
 
 let novcanica = [10,20,50,100,200,500]
 let broj_novcainca = novcanica.length
@@ -94,3 +94,35 @@ while(brojac < broj_novcainca){
 console.log(`Zbir svih brojeva je:${rez1}`)
 
 
+//vezba3
+let cars = ["audi","bmw","golf","seat","ferrari"]
+let broj_automobila = cars.length
+let brojacc = 0
+
+while(brojacc < broj_automobila){
+  console.log(cars[brojacc])
+  brojacc++
+}
+console.log("-----------")
+
+//vezba4
+
+let num = [2,3,5,64,234,413,41,412,12,4,241,142,325]
+let broj = num.length
+
+let counter = 0
+let rezultat1 = 0
+let rezultat2 = 0
+
+
+while(counter< broj){
+  if(num[counter] % 2 == 0){
+    rezultat1 = rezultat1 + num[counter]
+  }if(num[counter] % 5 == 0){
+    rezultat2 = rezultat2 + num[counter]
+  }
+  counter++;
+}
+
+console.log(`Zbir brojeva koji su deljivi sa 2 je ${rezultat1}`)
+console.log(`Zbir brojeva koji su deljivi sa 5 je ${rezultat2}`)

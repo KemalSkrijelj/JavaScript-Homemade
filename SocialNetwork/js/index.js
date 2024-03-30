1b340f7ae70af533aa85ef3be75e79e4add8eb52
Kemal Škrijelj <kemalskrijelj6@gmail.com>
2024-03-30 19:43:19 +0100

    1 step

---
+++ SocialNetwork/js/index.js
@@ -0,0 +1,6 @@
+document.querySelector('#registracija').addEventListener('click', () => {
+  document.querySelector('.custom-modal').style.display = 'block';
+})
+document.querySelector('#close-modal').addEventListener('click', () => {
+  document.querySelector('.custom-modal').style.display = 'none';
+})

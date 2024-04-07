38ca396c5a632109b5f60dc1cb0a9c02a511b1ae
Kemal Škrijelj <kemalskrijelj6@gmail.com>
2024-04-07 21:05:40 +0200

    5step

--- SocialNetwork/js/hexa.js
+++ SocialNetwork/js/hexa.js
@@ -35,10 +35,10 @@ document.querySelector('#close-modal').addEventListener('click', () => {
 document.querySelector('#editForm').addEventListener('submit', e => {
   e.preventDefault()
 
-  let user = new User();
-  user.username = document.querySelector('#korisnicko_ime').value;
-  user.email = document.querySelector('#edit_email').value;
-  user.edit();
+  let user1 = new User();
+  user1.username = document.querySelector('#korisnicko_ime').value;
+  user1.email = document.querySelector('#edit_email').value;
+  user1.edit();
 })
 
 document.querySelector('#deleteProfile').addEventListener('click', e => {
@@ -47,8 +47,8 @@ document.querySelector('#deleteProfile').addEventListener('click', e => {
   let text = 'Da li ste sigurni da zelite da izbrisete profil'
 
   if (confirm(text) === true) {
-    let user = new User()
-    user.delete()
+    let user2 = new User()
+    user2.delete()
   }
 })
 
@@ -57,10 +57,49 @@ document.querySelector("#postForm").addEventListener('submit',  e => {
 
   async function createPost() {
     let content = document.querySelector('#postContent').value;
+    document.querySelector('#postContent').value = ''
     let post = new Post()
-    post.content = content
+    post.post_content = content
     post = await post.create();
 
-    console.log(post)
+    let current_user = new User()
+    current_user = await current_user.get(session);
+    
+    let delete_post_html = '';
+
+    if (session === post.user_id) {
+      delete_post_html = ` <button class="remove-btn" onclick="removeMyPost(this)">Remove</button>` 
+    }
+
+    document.querySelector('#allPostsWrapper').innerHTML = `<div class="single-post" data-post_id="${post.id}"
+                                                             <div class="post-content">${post.content}</div>
+
+                                                              <div class = "post-actions">
+                                                                <p><b>Autor:</b> ${current_user.username}</p>
+                                                                <div>
+                                                                 <button onclick="likePost(this)" class="likePostJS like-btn"><span>${post.likes}</span>Likes</button>
+                                                                 <button class="comment-btn" onclick="commentPost(this)">Comments</button>
+                                                                   ${delete_post_html}
+                                                                 </div>
+                                                                </div?>
+
+                                                                 <div class="post-commments">
+                                                                     <form>
+                                                                       <input placeholder="Napisi komentar..." type="text">
+                                                                       <button onclick="commentPostSubmit(event)">Comment</button>
+                                                                     </form>
+                                                                 </div>
+                                                            </div>
+                                                            
+                                                            `
   }
+  createPost()
 })
+
+const commentPostSubmit = event => {
+
+}
+
+const removeMyPost = el => {
+
+} 

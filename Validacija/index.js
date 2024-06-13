1a6b5f526f9cce97682e9adcae03536a59879067
Kemal Škrijelj <149430750+KemalSkrijelj@users.noreply.github.com>
2024-06-13 15:30:45 +0200

    Update Validation

--- Validacija/index.js
+++ Validacija/index.js
@@ -1,41 +1,36 @@
-function validacija (){
-   let input = document.querySelector('#email');
-   let value = input.value
+function validacija  ()  {
+    let input = document.querySelector('#email');
+    let value = input.value;
 
-//Provera da li value(string) sadrzi @ i .
-if (value.includes('@')  && value.includes('.')) {
+    // Provera da li value(string) sadrzi @ i .
+    if (value.includes('@') && value.includes('.')) {
+        let pozicijaAt = value.indexOf('@'); // Proveramo poziciju @
+        let pozicijaTacka = value.indexOf('.'); // Proveramo poziciju .
 
-  let pozicijaAt = value.indexOf('@');//Proveramo poziciju @
-  let pozicijaTacka = value.indexOf('.');//Proveramo poziciju .
-  
-  //uzima vrednost izmedju pozicija @ i . i ne ukljucuje poslednju poziciju
-  let izmedjuAtTacka = value.substring(pozicijaAt + 1, pozicijaTacka)
-  
-  // da li izmedju . i @ ima vise od 0 karaktera
-  if (izmedjuAtTacka.lenght > 0) {
-    let preAt = value.substring(pozicijaTacka + 1, value.lenght)
-  
-  //Proveramo da li pre @ ima vise od 2 karaktera
-  if (preAt.lenght > 2) {
-    
-  let posleTacke = value.substring(pozicijaTacka + 1, value.lenght)  
+        // Uzimamo vrednost izmedju pozicija @ i . i ne ukljucuje poslednju poziciju
+        let izmedjuAtTacka = value.substring(pozicijaAt + 1, pozicijaTacka);
 
-  //proveravamo da li posle tacke ima vise od 1 karaktera
-  if (posleTacke.lenght > 1) {
-    console.log('Email je validan')
-  }else{
-    console.log('Email nije validan')
-  }
+        // Da li izmedju . i @ ima vise od 0 karaktera
+        if (izmedjuAtTacka.length > 0) {
+            let preAt = value.substring(0, pozicijaAt); // Ispravljeno: uzimamo vrednost pre @
 
-  }else{
-    console.log('Email nije validan')
-  }
-  }else{
-    console.log('Email nije validan')
-  }
+            // Proveravamo da li pre @ ima vise od 2 karaktera
+            if (preAt.length > 2) {
+                let posleTacke = value.substring(pozicijaTacka + 1); // Ispravljeno: uzimamo vrednost posle tacke
 
-  }else{
-  console.log('Email nije validan')
- }
-
-}
+                // Proveravamo da li posle tacke ima vise od 1 karaktera
+                if (posleTacke.length > 1) {
+                    console.log('Email je validan');
+                } else {
+                    console.log('Email nije validan');
+                }
+            } else {
+                console.log('Email nije validan');
+            }
+        } else {
+            console.log('Email nije validan');
+        }
+    } else {
+        console.log('Email nije validan');
+    }
+};
